76014ef40098ddfacba879a54ec6c8adcf750317
Berat <uberaty@gmail.com>
2017-09-12 17:42:01 +0930

    commit the back stall ticket changes

--- MyInitial/TicketsForm.Designer.cs
+++ MyInitial/TicketsForm.Designer.cs
@@ -169,6 +169,7 @@
         private System.Windows.Forms.CheckBox chkDiscount;
         private System.Windows.Forms.RadioButton radBalcony;
         private System.Windows.Forms.RadioButton radGeneral;
+        private System.Windows.Forms.RadioButton radBackStall;
         private System.Windows.Forms.RadioButton radBox;
         private System.Windows.Forms.GroupBox grpSection;
         private System.Windows.Forms.Button cmdCalculate;
